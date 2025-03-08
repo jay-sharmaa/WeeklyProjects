1dda1087793d927932eab98d78b433f1593a6c24
jay-sharmaa <sjaysharma00@gmail.com>
2025-03-08 21:55:22 +0530

    working needs analysis improvement

--- gitStrengthChecker/gitStrengthChecker.py
+++ gitStrengthChecker/gitStrengthChecker.py
@@ -4,9 +4,14 @@ r = requests.get('https://api.github.com/repos/jay-sharmaa/Fitlite/commits')
 
 text = r.json()
 
-size = len(text)
+additions = 0
+deletions = 0
 
-for i in range(0, size):
+for i in range(0, len(text)):
     curr_sha = text[i]['sha']
-    r = requests.get('https://api.github.com/repos/jay-sharmaa/Fitlite/commits/{curr_sha}')
-print(text)
+    r = requests.get(f'https://api.github.com/repos/jay-sharmaa/Fitlite/commits/{curr_sha}')
+    additions += r.json()['stats']['additions']
+    deletions += r.json()['stats']['deletions']
+
+print(additions)
+print(deletions)
