156ab480fc09fe112bd573572ef37ea50985944b
jay-sharmaa <sjaysharma00@gmail.com>
2025-03-09 14:40:47 +0530

    added graph representation for user ease

--- gitStrengthChecker/gitStrengthChecker.py
+++ gitStrengthChecker/gitStrengthChecker.py
@@ -1,17 +1,60 @@
 import requests
+import re
+import matplotlib.pyplot as plt
 
-r = requests.get('https://api.github.com/repos/jay-sharmaa/Fitlite/commits')
+type = int(input('Give User Link/Name and Repository -- 0/1: '))
+
+url = ''
+
+if type == 0:
+    user_name = input('Type User Name: ')
+    repo_name = input('Type Repository Name: ')
+    url = f'https://api.github.com/repos/{user_name}/{repo_name}/commits'
+elif type == 1:
+    url = input('Paste The Url Here: ')
+    result = re.search(r'https://github.com/([^/]+)/([^/]+)/commits', url)
+    url = f'https://api.github.com/repos/{result.group(1)}/{result.group(2)}/commits'
+    print(url)
+
+r = requests.get(url)
 
 text = r.json()
 
-additions = 0
-deletions = 0
+print(text)
 
-for i in range(0, len(text)):
+additions = []
+deletions = []
+
+x_axis = []
+for i in range(0, min(len(text), 12)):
+    x_axis.append(i)
+
+for i in range(0, min(len(text), 12)):
     curr_sha = text[i]['sha']
-    r = requests.get(f'https://api.github.com/repos/jay-sharmaa/Fitlite/commits/{curr_sha}')
-    additions += r.json()['stats']['additions']
-    deletions += r.json()['stats']['deletions']
+    
+    r = requests.get(f'{url}/{curr_sha}')
+    print(url + curr_sha)
+    commit_details = r.json()
 
-print(additions)
-print(deletions)
+    for file in commit_details['files']:
+        addition += file['additions']
+        deletion += file['deletions']
+        
+        if 'patch' in file:
+            print("Patch content for file:", file['filename'])
+            print()
+        
+    additions.append(addition)
+    deletions.append(deletion)
+
+plt.plot(x_axis, additions, color='blue', label='additions')
+plt.plot(x_axis, deletions, color='red', label = 'deletions')
+
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+
+plt.title('Project Relevance')
+
+plt.grid(True)
+
+plt.show()
